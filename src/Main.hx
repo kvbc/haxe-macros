1d8526a1fc76358a58a327499ae1cc19b31e6d23
Matt <64214044+kvbc@users.noreply.github.com>
2023-04-23 20:08:35 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -2,29 +2,29 @@ package;
 
 @:transitive
 abstract A(Int) from Int to Int {
-    @:op(A + B)
-    public function add (other: A): A {
-        return (this: Int) + (other: Int);
-    }
+	@:op(A + B)
+	public function add(other:A):A {
+		return (this : Int) + (other : Int);
+	}
 
-    @:op(A - B)
-    public function sub (other: A): A {
-        return (this: Int) - (other: Int);
-    }
+	@:op(A - B)
+	public function sub(other:A):A {
+		return (this : Int) - (other : Int);
+	}
 }
 
 @:transitive
-@:build(ForwardOpsMacro.build("B", "A"))
+@:build(ForwardOpsMacro.build())
 abstract B(A) from A to A {}
 
-@:build(ForwardOpsMacro.build("C", "B"))
+@:build(ForwardOpsMacro.build())
 abstract C(B) from B to B {}
 
 class Main {
 	static private function main() {
-        var a: C = 68;
-        var b: C = 1;
-        var c: C = a + b;
-        trace(c);
+		var a:C = 68;
+		var b:C = 1;
+		var c:C = a + b;
+		trace(c);
 	}
 }
